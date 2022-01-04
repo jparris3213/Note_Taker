d14faf4f969dc554354ddc230fec5275f0c26a27
Jacob Parris <typefortyproductions@gmail.com>
2022-01-03 19:16:45 -0500

    Initial Heroku Build Push

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,32 @@
 const note = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 note.get('/', (req,res) =>
 readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
+note.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if ( title && text ) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        res.json(response);
+    } else {
+        res.json('Did Not Post-It...The Note');
+    }
+});
 
 
 
